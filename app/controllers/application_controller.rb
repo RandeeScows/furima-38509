cd1dd39e1f2dc3de7b786023a5a68ae50101bed0
RandeeScows <carppooli3yuan1@gmail.com>
2023-02-04 18:51:33 +0900

    babel.config modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-      # admin, 2222
+      # admin, 2222　　e10f8b5487e8b7af56301b251d9ca238　a927e8964ea777dc6eca0e3ffed00aef
     end
   end  
 end
