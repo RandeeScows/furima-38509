3f57279d0f529effef1c8ef859a106ad1d9607aa
RandeeScows <carppooli3yuan1@gmail.com>
2023-02-15 01:17:22 +0900

    ユーザー登録
     必要な情報を適切に入力して「会員登録」ボタンを押すと、ユーザーの新規登録ができる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   private
-
+  
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
       # admin, 2222　　e10f8b5487e8b7af56301b251d9ca238　a927e8964ea777dc6eca0e3ffed00aef
     end
-  end  
+  end
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :family_name, :given_name, :phonetic_fn, :phonetic_gn, :birthday])
+  end
 end
