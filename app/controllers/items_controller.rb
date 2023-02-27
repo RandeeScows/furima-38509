8877443607d7f470d948b013cca1a7ac84229660
RandeeScows <carppooli3yuan1@gmail.com>
2023-02-28 02:26:09 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :move_to_index, except: [:index, :show] 
+  before_action :validate_user, only: [:edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -18,11 +20,43 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+    @user = User.find(@item.user_id)
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    @user = User.find(@item.user_id)
+  end 
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(
     :name, :image, :price, :description, :category_id, :condition_id,
     :shipping_cost_on_id, :origin_id, :lead_time_id
     ).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in? 
+      redirect_to action: :index
+    end
+  end
+
+  def validate_user
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
