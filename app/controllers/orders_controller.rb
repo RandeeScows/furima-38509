d2f309d61521af50cbc17f1677965ea151121770
RandeeScows <carppooli3yuan1@gmail.com>
2023-03-09 21:36:07 +0900

    purchase bug addressed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
-  before_action :set_order, only: [:index, :create]
   before_action :validate_user, only: [:index, :create]
 
   def index
@@ -30,13 +29,13 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address)
     .permit(:zip_code, :prefecture_id, :township, :block, :building, :tel)
-    .merge(item_id: @item.id, user_id: current_user.id, token: params[:token], price: @item[:price])
+    .merge(item_id: @item.id, user_id: current_user.id, token: params[:token])
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],
+      amount: @item[:price],
       card: order_params[:token],
       currency: 'jpy'
     )
